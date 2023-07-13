586c1dd0a2cdc7ce90c6b77fa924c36b85c4ceb2
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-07-13 23:16:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ GROUP = os.environ.get("GROUP", "vvslh_pro")
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL","-1001493654559"))
 
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001507337802"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001807113169"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
